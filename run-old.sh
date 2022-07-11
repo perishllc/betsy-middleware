cd575b70b8c5c3902b712ba932dcf2cc10081d07
fossephate <matt.cfosse@gmail.com>
2022-07-10 20:02:42 -0400

    save

---
+++ run-old.sh
@@ -0,0 +1,1 @@
+./venv/bin/python main.py --host 0.0.0.0 --port 5555 --precache --bpow-nano-difficulty --callbacks http://nautilus.perish.co:5076/callback --work-urls="http://worker1.perish.co"
---
+++ work-proxy.sh
@@ -0,0 +1,1 @@
+./venv/bin/python main.py --host 0.0.0.0 --port 5555 --precache --bpow-nano-difficulty --callbacks http://nautilus.perish.co/retro --work-urls="http://worker1.perish.co:7090 https://pow.nano.to"
